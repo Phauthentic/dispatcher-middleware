14179c178e3ab4f4b373ff0b142801c495319667
Florian Krämer <florian.kraemer@kreative-design.net>
2021-08-08 18:14:33 +0200

    Updating Phive

--- config/composer_phive.php
+++ config/composer_phive.php
@@ -6,13 +6,26 @@
  */
 
 $ds = DIRECTORY_SEPARATOR;
+$phiveUrl = 'https://github.com/phar-io/phive/releases/download/0.13.4/phive-0.13.4.phar';
 
 if (!file_exists('.' . $ds . 'phive.phar')) {
-    echo 'Downloading Phive (https://phar.io/)...' . PHP_EOL;
-	file_put_contents('.' . $ds . 'phive.phar', file_get_contents('https://phar.io/releases/phive.phar'));
+    echo 'Downloading Phive (' . $phiveUrl . ')...' . PHP_EOL;
+    file_put_contents('.' . $ds . 'phive.phar', file_get_contents(
+        $phiveUrl
+    ));
 }
 
-$output = '';
-exec('php .' . $ds . 'phive.phar install', $output);
+$keys = [
+    '0x4AA394086372C20A',
+    '0x8E730BA25823D8B5',
+    '0x31C7E470E2138192',
+    '0x4AA394086372C20A',
+    '0x0F9684B8B16B7AB0',
+    '0xBB5F005D6FFDD89E',
+    '0xCF1A108D0E7AE720'
+];
+$keys = implode(',', $keys);
 
+$output = '';
+exec('php .' . $ds . 'phive.phar install --target ./bin --trust-gpg-keys ' . $keys . ' --force-accept-unsigned --temporary', $output);
 echo implode(PHP_EOL, $output);
